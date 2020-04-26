f0c5e9197a5c59125f646b4535e6d510884bfbe2
Hibari <1104709426@qq.com>
2020-04-26 17:15:02 +0800

    1.1
    更新了四元数插值和旋转矩阵计算
    更新了readme.txt

---
+++ project.py
@@ -0,0 +1,33 @@
+
+
+from numpy import *
+from math import *
+
+
+def rotation_matrix(pitch, rol, yaw):  # 输入参数计算旋转矩阵
+    R = np.zeros((3, 3))
+    R[0, 0] = cos(pitch)*cos(yaw)-sin(pitch)*sin(rol)*sin(yaw)
+    R[0, 1] = -cos(pitch)*sin(yaw)-sin(pitch)*sin(rol)*cos(yaw)
+    R[0, 2] = -sin(pitch)*cos(rol)
+    R[1, 0] = cos(rol)*sin(yaw)
+    R[1, 1] = cos(rol)*cos(yaw)
+    R[1, 2] = -sin(rol)
+    R[2, 0] = sin(pitch)*cos(yaw)+cos(pitch)*sin(rol)*sin(yaw)
+    R[2, 1] = -sin(pitch)*sin(yaw)+cos(pitch)*sin(rol)*cos(yaw)
+    R[2, 2] = cos(pitch)*cos(rol)
+    return R
+
+
+def q_interpolation(att1, att2, t, flag=0):  # 用读取的四元数数据（包含时间）得到内插完的四元数
+    if flag == 0:
+        q0 = array([att1[1], att1[2], att1[3], sqrt(1-pow(att1[1], 2)-pow(att1[2], 2)-pow(att1[3], 2))])
+        q1 = array([att2[1], att2[2], att2[3], sqrt(1-pow(att2[1], 2)-pow(att2[2], 2)-pow(att2[3], 2))])
+    else:
+        q0 = array(att1[1:])
+        q1 = array(att2[1:])
+    t0 = float(att1[0])
+    t1 = float(att2[0])
+    theta = acos(sum(multiply(q0, q1)))
+    y0 = sin(theta*(t1-t)/(t1-t0))/sin(theta)
+    y1 = sin(theta*(t-t0)/(t1-t0))/sin(theta)
+    return y0*q0+y1*q1
--- readme.txt
+++ readme.txt
@@ -5,20 +5,30 @@ flag设为1时，用于读取仿真数据，返回[(timeCode, q1, q2, q3, q4),..
 
 read_gps(filename="./data/DX_ZY3_NAD_gps.txt")
 用于读取gps数据
-返回[(timCode, PX, PY, PZ, VX, VY, VZ),...]
+返回[(timCode, PX, PY, PZ, VX, VY, VZ),...]（array）
 
 read_time(filename="./data/DX_ZY3_NAD_imagingTime.txt", flag=0)
 用于读取时间数据文件
 flag默认值为0，用于读取资源三数据
 flag设为1时，用于读取仿真数据
-返回[(t, dt),...]
+返回[(t, dt),...]（array）
 
 read_cbr(filename="./data/NAD.cbr", flag=0)
 用于读取视向量文件
 flag默认值为0，用于读取资源三数据
 flag设为1时，用于读取仿真数据
-返回值为[(φy, φx),...]
+返回值为[(φy, φx),...]（list）
 
 read_nad(filename="./data/NAD.txt")
 用于读取相机相对卫星本体的姿态参数
-返回值为[(pitch, Vpitch, roll, Vroll, yaw, Vyaw),...]
+返回值为[(pitch, Vpitch, roll, Vroll, yaw, Vyaw),...](list)
+
+rotation_matrix(pitch, rol, yaw)
+用于计算旋转矩阵
+返回旋转矩阵（array）
+
+q_interpolation(att1, att2, t, flag=0)
+用于进行四元数插值
+att1 att2分别为读取数据文件得到的包含时间信息的数据条
+flag默认为0 读取仿真文件设置为1
+返回四元数(q1,q2,q3,q4)(array)
