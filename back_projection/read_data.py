a97223eb3923ef64ed35fba7bf7b73e0ce1ddb98
ClaudiaShu <48800547+ClaudiaShu@users.noreply.github.com>
2020-05-13 23:57:38 +0800

    Update read_data.py

--- back_projection/read_data.py
+++ back_projection/read_data.py
@@ -2,9 +2,6 @@ import numpy as np
 import cv2
 # from libtiff import TIFF
 
-
-
-
 #get att check
 def read_att(filename="./data/DX_ZY3_NAD_att.txt"):
     with open(filename) as f:  # with语句自动调用close()方法
@@ -30,10 +27,12 @@ def read_att(filename="./data/DX_ZY3_NAD_att.txt"):
             data.append([timeCode, q1, q2, q3, q4])
 
             f.readline()
-        return data
+    # print(data[0])
+    return data
 
 #get gps check
 def read_gps(filename="./data/DX_ZY3_NAD_gps.txt"):
+    
     with open(filename) as f:  # with语句自动调用close()方法
         for i in range(4):
             f.readline()
@@ -54,23 +53,36 @@ def read_gps(filename="./data/DX_ZY3_NAD_gps.txt"):
             VX = float((f.readline().lstrip()).split(' ')[2])
             VY = float((f.readline().lstrip()).split(' ')[2])
             VZ = float((f.readline().lstrip()).split(' ')[2])
-            data.append([timeCode, PX, PY, PZ, VX, VY, VZ])
+            
+            data.append([timeCode, PX, PY, PZ])
 
             f.readline()
-        return data
+        
+    gpsmat = np.zeros((data_num,3))
+    T = np.zeros(data_num)
+    # gpsmat = data[0:data_num,1:3]
+    # T = data[0:data_num,0]
+    for i in range(data_num):
+        T[i] = data[i][0]
+        gpsmat[i][0] = data[i][1]
+        gpsmat[i][1] = data[i][2]
+        gpsmat[i][2] = data[i][3]
+    # print(gpsmat)
+    # print(T)
+
+    return T,data,gpsmat
 
 #get time check
 def read_time(filename="./data/DX_ZY3_NAD_imagingTime.txt"):
     f = open(filename, 'r')
     data = []
-
-    f.readline() 
+    # f.readline() 
     for lines in f:
         x = float(lines.split('\t')[1])
-        y = float(lines.split('\t')[2])
+        # y = float(lines.split('\t')[2])
 
-        data.append([x, y])
-
+        data.append(x)
+    # print(data[0])
     f.close()
     return data
 
@@ -84,9 +96,10 @@ def read_cbr(filename="./data/NAD.cbr"):
         x = float(lines.split('\t')[1])
         y = float(lines.split('\t')[2])
 
-        data.append([x, y])
+        data.append([x, y, 1])
 
     f.close()
+    # print(data,data.__len__())
     return data
 
 #get rotation matrix from J2000 to WGS84
@@ -94,6 +107,8 @@ def read_J2W(filename="./data/J2WGS.txt"):
     f = open(filename, 'r')
     time = []
     R = []
+    rmat = np.zeros((12,3))
+    T = np.zeros(4)
 
     for lines in f:
         timeCode = float(lines.split(' ')[2])
@@ -113,12 +128,16 @@ def read_J2W(filename="./data/J2WGS.txt"):
 
         time.append(timeCode)
         R.append(r)
-    T = np.zeros(4)
+    
     for i in range(4):
         T[i] = time[i]
+        rmat[i*3:i*3+3,0:3]=R[i]
 
     f.close()
-    return T,R
+
+    # print(rmat)
+
+    return T,R,rmat
 
 #获得像点坐标 读取DEM tiff文件
 def get_XYZ(filename="./data/SRTM_mosaic.tif"):
@@ -126,15 +145,17 @@ def get_XYZ(filename="./data/SRTM_mosaic.tif"):
 
     #####test data######
     data = []
-    data.append([1934303.0,743483.0,5297.0])
-    data.append([2343434.0,798337.0,675.0])
-    data.append([0.0,0.0,0.0])
-
-
+    data.append([-2282126.083,5054348.290,3142026.214])
+    data.append([-2214395.2, 5882759.5, 2780867.2])
+    data.append([-2377798.3431889759,5161197.8955926420,1083479.4051405331])
 
     return data
 
 if __name__ == "__main__":
     # get_XYZ()
     # read_J2W()
+    # read_gps()
+    # read_time()
+    # read_att()
+    # read_cbr()
     pass
